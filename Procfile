83dd0862859484cd7b851b98f275e286d0889fca
Jeremy E. <0001077192@ccisd.net>
2018-05-15 16:34:40 -0500

    First (test) export

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: java -jar target/spicybot-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/com/nsa/spicybot/SpicyBot.java
@@ -0,0 +1,43 @@
+package com.nsa.spicybot;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import net.dv8tion.jda.core.hooks.SubscribeEvent;
+
+public class SpicyBot extends ListenerAdapter
+{
+	public static JDA discord;
+	private static String token, guild, channel;
+	
+    public static void main( String[] args )
+    {
+    	token   = args[0];
+    	guild   = args[1];
+    	channel = args[2];
+    	
+    	JDABuilder builder = new JDABuilder( AccountType.BOT );
+    	builder.addEventListener( new SpicyBot() );
+    	try {
+			discord = builder.buildBlocking();
+	    	discord.getGuildById( guild ).getTextChannelById( channel ).sendMessage( "I'm online! Hello @everyone!" );
+		} catch( LoginException e )
+    	{
+			e.printStackTrace();
+        } catch( InterruptedException e )
+    	{
+			e.printStackTrace();
+		}
+    }
+    
+    @SubscribeEvent
+    public void onMessageReceived( MessageReceivedEvent evt )
+    {
+    	if( evt.getChannel().getId().equals( channel ) )
+    		evt.getChannel().sendMessage( "_spicy_" );
+    }
+}
